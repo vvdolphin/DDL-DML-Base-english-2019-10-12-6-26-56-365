a74dbb5c65a6f670124da2d621938e80c92cdd4d
vvdolphin <1273027520@qq.com>
2020-07-01 22:24:14 +0800

    DDLandDML

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,60 @@
 -- **Database level:**
 -- Display all the database
 
+show databases;
+
 -- Enter a certain database
+ 
+ use name;
 
 -- Create a database
 
+create database name;
+
 -- Create the database of the designated character set
+
+create database name character set utf-8
 
 -- Display the creation information fo the database
 
+SHOW CREATE DATABASE name;
+
 -- Revise the codes of the database
 
+alter database name character set utf8;
+
 -- Delete a database
+drop database name;
 
 -- **Table level**
 -- Revise table name
 
+alter table name rename to name1;
+
 -- Revise the field's data type
+
+alter table name modify field char(10);
 
 -- Revise field name
 
+alter table name change fieldName1 fieldName2 char(10);
+
 -- Add field
+
+alter table name add(fileName varchar(10))
 
 -- Delete field
 
+alter table name drop fileName;
+
 -- Revise the table's storage engine
+
+ALTER TABLE name ENGINE = InnoDB;
 
 -- Delete the table's foreign key restriant
 
+alter table name drop foreign key foreignKeyName;
+
 -- Delete a table
+
+drop table name;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,14 @@
 -- Insert record
 
+insert into student(id,name,age,sex) values(003,"kk",20,female);
+
 -- Revise record
+
+update student set age = 20 where id = 001;
 
 --  Delete record
 
+delete from student where id = 001;
+
 -- Search record
+select * from student where id = 001;
